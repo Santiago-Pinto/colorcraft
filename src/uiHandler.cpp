5b15aeacddc71abc2728ea1013967c52702c2082
Santiago <pinto.santiago.augusto@gmail.com>
2020-07-10 20:44:34 -0300

    Removed unnecessary cout

--- src/uiHandler.cpp
+++ src/uiHandler.cpp
@@ -36,12 +36,10 @@ void UIHandler::setGridTitles(Grid*& grid, QStringList& titles) {
   this->gridHandler.setTitles(grid, titles);
 }
 
-#include <iostream>
-using namespace std;
+
 void UIHandler::displayColoring(Grid*& grid, QComboBox*& cmbBox) {
   string course = this->cmbHandler.getValue(cmbBox);
   Coloring courseColoring = system.getColoring().getCourseColoring(course);
-  cout<<"dafhajfhadjfgdalfgad;fa"<<endl;
   if (!courseColoring.getFunctional())
     uiMessageHandler.displayWarning("No se pudo encontrar un horario valido "
                                     "para el limite de bloques solicitado");
